91f45d0ab58232c62cc417e618fac85538cea9ff
IvanShyvanau <kippbelarus@gmail.com>
2021-06-03 22:01:06 +0300

    nn

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin(), // HTML подключаем
+  ],
+};
